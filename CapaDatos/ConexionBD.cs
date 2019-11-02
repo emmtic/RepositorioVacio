5b705b86f081eebd9a39dc2258c98f1ff1e8cdc3
emmtic <gitemmtic@gmail.com>
2019-11-02 16:52:40 -0300

    Primer Commit subiendo solucion vacia

---
+++ CapaDatos/ConexionBD.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class ConexionBD
+    {
+       /* private MySqlConnection Conexion = new MySqlConnection("Server=localhost; DataBase=libromedico; User=root; Password=");
+
+        public MySqlConnection AbrirConexion()
+        {
+            if (Conexion.State == ConnectionState.Closed) Conexion.Open(); return Conexion;
+        }
+
+        public MySqlConnection CerrarConexion()
+        {
+            if (Conexion.State == ConnectionState.Open) Conexion.Close(); return Conexion;
+        }
+        */
+    }
+}
