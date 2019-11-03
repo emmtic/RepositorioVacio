97bb7c9a79b27dad25ffc0c3d9986471504c9d32
Emmanuel Gimenez <lemmanuel.gimenez@gmail.com>
2019-11-03 02:02:25 -0300

    Empiezo con parte de Usuario
    Se agrega Nivel de Capas 1: Datos- 2:Cita Medica- 3: Presentacion

---
+++ CapaPresentacion/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using CapaPresentacion.Vistas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CapaPresentacion
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            CP_Usuario Ventana = new CP_Usuario();
+            Ventana.ShowDialog();
+        }
+    }
+}
